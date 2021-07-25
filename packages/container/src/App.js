a8430c3ec6e6ac258214c51f3d3bfe656082a8d6
mrdjan-stajic-ey <mrdjan.stajic@rs.ey.com>
2021-07-25 15:28:33 +0200

    init commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+
+export default () => (<div>
+    <h1>Container app header</h1>
+    <hr />
+    <MarketingApp />
+</div>);
---
+++ packages/marketing/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/styles';
+
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
+
+export default () => {
+    return (
+        <>
+            <StylesProvider>
+                <Router>
+                    <Switch>
+                        <Route exact path="/pricing" component={Pricing} />
+                        <Route path="/" component={Landing} />
+                    </Switch>
+                </Router>
+            </StylesProvider>
+        </>
+    )
+}
